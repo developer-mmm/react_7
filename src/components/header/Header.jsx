a3696c4dd37e50e70452506937a4e26fe50952b9
developer-mmm <ahmadahmadaliyev7@gmail.com>
2024-06-06 23:58:39 +0500

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className="bg-base-200">
+      <div className="navbar site-container">
+        <div className="navbar-start">1</div>
+        <div className="navbar-center">2</div>
+        <div className="navbar-end">3</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
